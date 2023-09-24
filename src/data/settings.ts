9cd5de300ef45bdad31d5e50bfc7e0ed656cd13f
Bran <111070465+panjipangweb@users.noreply.github.com>
2023-09-24 23:06:07 +0700

    Update settings.ts

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
   site: 'https://blogpanji.netlify.app/',
-  name: 'Personal Blog. Thanks to <a href="https://blog.framer.website/">Framer Website</a>',
+  name: 'Personal trash to keep my thoughts</a>',
   title: 'Site title',
   description: 'Site description',
   showLittleSticksPlug: true,
