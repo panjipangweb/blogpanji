9b858927979f73d2408c7a5cc1bb0db155d0a675
Bran <111070465+panjipangweb@users.noreply.github.com>
2023-09-24 23:16:34 +0700

    Update settings.ts

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
   site: 'https://blogpanji.netlify.app/',
-  name: 'Personal trash to keep my thoughts</a>',
+  name: 'Personal trash to keep my thoughts hehe',
   title: 'Site title',
   description: 'Site description',
   showLittleSticksPlug: true,
